6ff2d6991a324e99933aadb606635e069cedd790
sandeepmchary <awssandeepchary@gmail.com>
2021-10-31 14:52:47 +0530

    added files

---
+++ giit.sh
@@ -0,0 +1,4 @@
+git add .
+git status
+git commit -m "added files"
+git push origin master
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "vpc_cidr" {
-  default = "10.0.1.0/16"
+  default = "10.0.1.0/24"
   type = string
 }
