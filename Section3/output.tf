1e8f879b482358c2ead599509149f049359871ab
sandeepmchary <awssandeepchary@gmail.com>
2021-11-02 23:37:23 +0530

    changed values in output

--- Section3/output.tf
+++ Section3/output.tf
@@ -2,11 +2,14 @@ output "vpc_cidr" {
   value = var.vpc_cidr
 }
 output "aws_subnet" {
-  value = aws_subnet.public
+  value = aws_subnet.public[count.index]
 }
 output "vpc_id" {
   value = aws_vpc.myjavavpc.id
 }
 output "vpc_owner_id" {
   value = aws_vpc.myjavavpc.owner_id
+}
+output "PublicRT" {
+  value = aws_route_table.PublicRT
 }
