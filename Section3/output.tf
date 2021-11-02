d194dcb270e7790a83354fb4793ea8ec07ae088c
sandeepmchary <awssandeepchary@gmail.com>
2021-11-02 23:40:10 +0530

    removed the subnet values

--- Section3/output.tf
+++ Section3/output.tf
@@ -1,9 +1,9 @@
 output "vpc_cidr" {
   value = var.vpc_cidr
 }
-output "aws_subnet" {
+/*output "aws_subnet" {
   value = aws_subnet.public[count.index + 1]
-}
+}*/
 output "vpc_id" {
   value = aws_vpc.myjavavpc.id
 }
